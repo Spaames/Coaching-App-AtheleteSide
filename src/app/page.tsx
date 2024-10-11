8c8ccc9f1387429359f8d4f541c5b6d0ec1080d6
Rémi Dupuy <rdu@mac-1.home>
2024-10-11 17:21:48 +0200

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+//will be rendered when the user visits the root of the application (/).
+export default function Page() {
+    return (
+        <>
+            <h1>Hello, Welcome to Next.js!</h1>
+            <p>This is the default page, the server is working. Please follow a route to use the application</p>
+        </>
+    )
+}
